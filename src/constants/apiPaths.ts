828f6cda6ee6c859a440a8b008f35fe417d84011
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2020-11-08 20:46:14 +0300

    change api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  products: 'https://hu3on2ee5i.execute-api.eu-west-1.amazonaws.com/dev/getProducts',
+  products: 'https://hu3on2ee5i.execute-api.eu-west-1.amazonaws.com/dev/products',
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev/',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
